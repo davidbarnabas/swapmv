3b3e9ad9dd1c9f184f72c49ca3c35b4e1000c9df
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-03-07 14:51:41 +0530

    Update 65.c

--- 65.c
+++ 65.c
@@ -3,13 +3,19 @@ def stair():
 	l=[]
 	n=int(input())
 	for i in range(n):
+
 		l.append(int(input()))
 	for i in l:
 		s+=(n-i)
+			
 	print(s)
+			
   
 try:
+
   stair()
+	  
 except:
+
   print('invalid')
   
